4feb092c5ce22f8a8939c378bf6d4bbd6f0ff2fd
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2016-06-16 10:52:31 +0500

    added all files (v 1.0)
    completed and tested the plugin locally.

---
+++ background.js
@@ -0,0 +1,44 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.executeScript(null, {
+        file: "highlight.js"
+    })
+});
+
+chrome.tabs.onActivated.addListener(function () {
+    var ba = chrome.browserAction;
+    ba.setBadgeBackgroundColor({
+        color: [255, 255, 255, 0]
+    });
+    ba.setBadgeText({
+        text: ''
+    });
+});
+
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    // enable or disable badge depending upon message from content_script
+    var ba = chrome.browserAction;
+    if (message === "enable") {
+        ba.setBadgeBackgroundColor({
+            color: [0, 255, 0, 128]
+        });
+        ba.setBadgeText({
+            text: '!'
+        });
+        chrome.notifications.create({
+            type: "basic",
+            iconUrl: "icons/not-so-simple128.png",
+            title: "No so Simple!",
+            message: "Hey there! You've been notified!"
+        });
+    } else if (message === "disable") {
+        ba.setBadgeBackgroundColor({
+            color: [255, 255, 255, 0]
+        });
+        ba.setBadgeText({
+            text: ''
+        });
+
+    }
+
+});
---
+++ highlight.js
@@ -0,0 +1,13 @@
+function highlight(container, what, spanClass) {
+    var content = container.innerHTML,
+        pattern = new RegExp('(>[^<.]*)(' + what + ')([^<.]*)', 'gi'),
+        replaceWith = '$1<span ' + (spanClass ? 'class="' + spanClass + '"' : '') + '">$2</span>$3',
+        highlighted = content.replace(pattern, replaceWith);
+    return (container.innerHTML = highlighted) !== content;
+}
+
+if (highlight(document.body, 'simple', 'highlight-simple')) {
+    chrome.runtime.sendMessage('disable');
+} else {
+    chrome.runtime.sendMessage('enable');
+}
